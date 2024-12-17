e8af0f3bb4359d8643b041cf44423b17ac055f65
Mohd. Nisab <nisabmohd@gmail.com>
2024-12-17 17:32:22 +0530

    fixed deployment issue

--- app/routes/pokemon.tsx
+++ app/routes/pokemon.tsx
@@ -1,16 +1,28 @@
 import { getPokenByName } from "@/lib/data";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import type { Route } from "./+types/pokemon";
 
 export async function loader({ params }: Route.LoaderArgs) {
-  return await getPokenByName(params.name);
+  const data = await getPokenByName(params.name);
+  if (!data) throw new Response("Not Found", { status: 404 });
+  return data;
 }
 
-export default function PokemonPage({ loaderData }: Route.ComponentProps) {
-  if (!loaderData) throw new Response("Not Found", { status: 404 });
+export default function PokemonPage({
+  loaderData: pokemon,
+}: Route.ComponentProps) {
   return (
     <div>
-      <title>{loaderData.name}</title>
-      {loaderData.name}
+      <title>{pokemon.name}</title>
     </div>
   );
 }
